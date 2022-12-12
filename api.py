165aee1f98b98efee01909e72a0888ce4d9229c9
Brian Li <brianli@master-dynamic.com>
2022-12-12 21:17:37 +0800

    foo

--- api.py
+++ api.py
@@ -53,7 +53,7 @@ def getHeatmap(paramJson=""):
         }
     res = getAPIbyJson(paramJson, "eventsources.cgi")
     resJson = [f"\"{x.split('=')[0]}\":\"{x.split('=')[1]}\"," for x in res.text[:-1].split("\r\n")][:-1]
-    resJson = "{" + resJson + "}"
+    resJson = "{" + "".join(resJson) + "}"
     resJson = json.loads(resJson)
     return resJson
 
