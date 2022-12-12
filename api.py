87940baac7c00d2bf80b5a4facce25ffa964675b
Brian Li <brianli@master-dynamic.com>
2022-12-12 20:25:10 +0800

    init

---
+++ api.py
@@ -0,0 +1,28 @@
+import requests
+
+baseUrl = "http://192.168.3.22"
+
+def jsonToRequestUrl(json, fileName):
+	returnValue = f"/{fileName}?"
+	for item in json:
+		returnValue += item + "=" + json[item] + "&"
+	if returnValue[-1] == "&":
+		returnValue = returnValue[:-1]
+	return returnValue
+
+
+
+def getRequest(url):
+    return requests.get(url)
+
+
+def postRequest(url, json, headers):
+    return requests.post(url, json=json, headers=headers)
+
+def getHeatmap():
+    paramJson = {
+        "action": "view",
+    }
+    url = baseUrl + jsonToRequestUrl(paramJson, "/stw-cgi/eventsources.cgi")
+    res = getRequest(url)
+    return res
---
+++ main.py
@@ -0,0 +1,7 @@
+from api import *
+def main():
+	print(getHeatmap().text)
+
+
+if (__name__ == "__main__"):
+    main()
