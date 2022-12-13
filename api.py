70592f1df56c6168b03be13427c0e8804b4c54f2
Brian Li <brianli@master-dynamic.com>
2022-12-13 18:06:50 +0800

    foo

--- api.py
+++ api.py
@@ -152,7 +152,7 @@ def rtspStream():
             jsonHeaders = [name for name in getHeatmap()]
             heatmapJson = getHeatmap()
             levels = heatmapJson[jsonHeaders[0]]
-            heatmapResolution = heatmapJson[jsonHeaders[2].split("x")]
+            heatmapResolution = heatmapJson[jsonHeaders[2]].split("x")
             levelsNp = np.reshape(levels, heatmapResolution)
             img = cv2.applyColorMap(levelsNp, cv2.COLORMAP_JET)
             cv2.imshow('image_display', img)
