f8bc461dee53a6108db76682262fbe98ad45d38a
Brian Li <brianli@master-dynamic.com>
2022-12-13 15:40:26 +0800

    foo

--- api.py
+++ api.py
@@ -112,7 +112,7 @@ def showCameraStream(paramJson=""):
     url = getUrlPath(paramJson, "video.cgi")
     stream = requests.get(url, stream=True, auth=auth)
     if stream.ok:
-        boundary = b'\r\n'
+        boundary = b'\r\n\r\n'
         buffer = b''
         for chunk in stream.iter_content():
             if(boundary not in buffer):
