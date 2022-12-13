ff4e5265e6bac6fe9021e15e4b678c880a68ec1d
Brian Li <brianli@master-dynamic.com>
2022-12-13 17:11:51 +0800

    foo

--- api.py
+++ api.py
@@ -148,6 +148,7 @@ def showCameraStream(paramJson=""):
                         timeAnchor = [startTime, downloadedAllbufferTime, foundAllPointTime, startParseStringTime, endParseStringTime, endDecodeImageTime]
                         timeAnchor = [timeAnchor[x]-timeAnchor[x-1] for x in range(len(timeAnchor))]
                         timeAnchor[0] = 0
+                        timeAnchor.append(len(imageNumpy))
                         print(timeAnchor)
                     startTime = time.time()
                     buffer = b''
