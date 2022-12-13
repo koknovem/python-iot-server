4385c4c0452f707c150e450e3b5cb9fe3aa0096d
Brian Li <brianli@master-dynamic.com>
2022-12-13 10:02:11 +0800

    added index and heatmap controller

--- controller.py
+++ controller.py
@@ -1,6 +1,15 @@
 from api import *
 from flask import Flask
 app = Flask(__name__)
+
 @app.route("/")
+def index():
+    return getPeoplecount()
+
+@app.route("/peoplecount")
 def peoplecount():
     return getPeoplecount()
+
+@app.route("/heatmap")
+def heatmap():
+    return getHeatmap()
