cb4c62d1c7dfd61d7a20406464d2f6c54f565950
Brian Li <brianli@master-dynamic.com>
2022-12-13 17:47:40 +0800

    foo

--- api.py
+++ api.py
@@ -149,6 +149,7 @@ def rtspStream():
     while True:
         ret, image = vidCap.read()
         if ret:
+            print(image)
             cv2.imshow('image_display', image)
             cv2.waitKey(10)
         else:
