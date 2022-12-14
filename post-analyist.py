36223f295f908e1e3950eeb1f148028955d6bb8b
Brian Li <brianli@master-dynamic.com>
2022-12-14 17:54:20 +0800

    foo

---
+++ post-analyist.py
@@ -0,0 +1,17 @@
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+files = [f for f in listdir("heatmap/") if isfile(join("heatmap/", f))]
+def readFile(file):
+    return np.loadtxt(file)
+def parseFmtToFloat(fmt):
+    pass
+def findNumpyDifference(anp, bnp):
+    pass
+def main():
+    for file in files:
+        content = readFile(file)
+        print(content)
+
+if __name__ == "__main__":
+    main()
