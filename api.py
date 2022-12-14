12f037f01877614fb1945bf3742d3e67f7619cad
Brian Li <brianli@master-dynamic.com>
2022-12-14 10:21:20 +0800

    foo

--- api.py
+++ api.py
@@ -89,7 +89,7 @@ def getHeatmapNumpy(paramJson=""):
     jsonHeaders = [name for name in resJson]
     levels = [int(x) for x in resJson[jsonHeaders[0]].split(",")]
     heatmapResolution = [int(x) for x in resJson[jsonHeaders[2]].split("x")]
-    levelsNp = np.reshape(levels, heatmapResolution)
+    levelsNp = np.reshape(levels, heatmapResolution).astype(np.uint8)
     img = cv2.applyColorMap(np.kron(levelsNp, np.ones((10,10)))/255, cv2.COLORMAP_JET)
     cv2.imshow('heatmap', img)
     return levelsNp
