6a220e03eedcb597deeaf7c435da26fa29a42888
Brian Li <brianli@master-dynamic.com>
2022-12-14 10:49:47 +0800

    foo

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     while True:
         heatmapNumpy = getHeatmapNumpy()
         heatmapshow = None
-        heatmapshow = cv2.normalize(heatmapNumpy, heatmapshow, alpha=0, beta=255, norm_type=cv2.NORM_MIXMAX, dtype=cv2.CV_8U)
+        heatmapshow = cv2.normalize(heatmapNumpy, heatmapshow, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
         heatmapshow = cv2.applyColorMap(heatmapshow, cv2.COLORMAP_JET)
         cv2.imshow("heatmap", heatmapshow)
         time.sleep(1)
