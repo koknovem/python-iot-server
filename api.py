8dfeeac570b669b35936065fa370fc59a4fc64ca
Brian Li <brianli@master-dynamic.com>
2022-12-14 17:25:03 +0800

    foo

--- api.py
+++ api.py
@@ -88,6 +88,7 @@ def getHeatmapNumpy(paramJson={}):
     resJson = resJson[headers[0]][0]
     jsonHeaders = [name for name in resJson]
     levels = resJson[jsonHeaders[1]]
+    np.savetxt("heatmap/" + datetime.datetime.now().strftime("%H-%M-%S") + ".csv", levels, delimiter=",")
     levels = [linearMapper(x, 0, 9999, 255, 0) for x in levels]
     heatmapResolution = [int(x) for x in resJson[jsonHeaders[3]].split("x")]
     levelsNp = np.reshape(levels, heatmapResolution).astype(np.uint8)
@@ -96,7 +97,6 @@ def getHeatmapNumpy(paramJson={}):
 def getHeatmapHeatmapImage():
     heatmapNumpy, heatmapResolution = getHeatmapNumpy()
     heatmapImage = cv2.applyColorMap(heatmapNumpy, cv2.COLORMAP_JET)
-    np.savetxt("heatmap/" + datetime.datetime.now().strftime("%H-%M-%S") + ".csv", heatmapNumpy, delimiter=",")
     return heatmapImage, heatmapResolution
 
 def getPeoplecount(paramJson={}):
