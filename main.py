f0926e9ad872832272f5687071c24ba263c8c20e
Brian Li <brianli@master-dynamic.com>
2022-12-14 11:16:30 +0800

    foo

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import cv2
 def main():
     while True:
         heatmapNumpy = getHeatmapNumpy()
-        heatmapNumpy = np.kron(heatmapNumpy, np.ones((2,2))).astype(cv2.uint8)
+        heatmapNumpy = np.kron(heatmapNumpy, np.ones((2,2))).astype(np.uint8)
         for row in heatmapNumpy:
             print(row)
         grayImage = cv2.cvtColor(heatmapNumpy, cv2.COLOR_GRAY2BGR)
