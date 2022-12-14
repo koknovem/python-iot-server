e651e27fc9fbb1eeb50b8f83b2603bc4f75eb187
Brian Li <brianli@master-dynamic.com>
2022-12-14 10:29:36 +0800

    foo

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import cv2
 def main():
     while True:
         heatmapNumpy = getHeatmapNumpy()
-        cv2.imshow(heatmapNumpy)
+        cv2.imshow(heatmapNumpy, cv2.COLORMAP_JET)
 
 
 if (__name__ == "__main__"):
