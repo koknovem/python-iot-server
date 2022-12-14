94c0a533085049a9ad6b3daa922e2dd6362bde23
Brian Li <brianli@master-dynamic.com>
2022-12-14 11:36:24 +0800

    foo

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ def main():
     while True:
         heatmapImage = getHeatmapHeatmapImage()
         heatmapImage = cv2.resize(heatmapImage,(500,500) ,interpolation=cv2.INTER_CUBIC)
-        cv2.imshow("heatmap", heatmapImage)
-        cv2.waitKey(1)
+        plt.imshow(heatmapImage)
+        # cv2.imshow("heatmap", heatmapImage)
+        # cv2.waitKey(1)
 
 
 if (__name__ == "__main__"):
