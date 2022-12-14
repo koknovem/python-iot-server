4161e2c0238115d6911534f6ba5aff12609f5a9b
Brian Li <brianli@master-dynamic.com>
2022-12-14 16:00:03 +0800

    foo

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     while True:
         multiplier = 4
         heatmapImage, heatmapResolution = getHeatmapHeatmapImage()
-        heatmapImage = cv2.resize(heatmapImage, (heatmapResolution[0]*multiplier, heatmapResolution[0]*multiplier), interpolation=cv2.INTER_CUBIC)
+        heatmapImage = cv2.resize(heatmapImage, (heatmapResolution[0]*multiplier, heatmapResolution[1]*multiplier), interpolation=cv2.INTER_CUBIC)
         plt.imshow(heatmapImage)
         plt.show()
         # cv2.imshow("heatmap", heatmapImage)
