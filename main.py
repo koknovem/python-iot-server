6990c0674df575505bb48c3db17092f70cf0776e
Brian Li <brianli@master-dynamic.com>
2022-12-14 11:05:17 +0800

    foo

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def main():
     while True:
         heatmapNumpy = getHeatmapNumpy()
         heatmapNumpy = heatmapNumpy.astype('uint8')
-        cv2.imshow("heatmap", heatmapNumpy)
+        grayImage = cv2.cvtColor(heatmapNumpy, cv2.COLOR_GRAY2BGR)
+        cv2.imshow("heatmap", grayImage)
         time.sleep(1)
 
 
