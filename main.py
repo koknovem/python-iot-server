92296edd7818e9aba115c24bc7563f16723a7355
Brian Li <brianli@master-dynamic.com>
2022-12-15 10:26:56 +0800

    foo

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ import cv2
 
 
 def main():
-    while True:
-        multiplier = 4
-        heatmapImage, heatmapResolution = getHeatmapHeatmapImage()
-        heatmapImage = cv2.resize(heatmapImage, (heatmapResolution[1]*multiplier, heatmapResolution[0]*multiplier), interpolation=cv2.INTER_CUBIC)
-        # plt.imshow(heatmapImage)
-        # plt.title(datetime.datetime.now().strftime("%H:%M:%S"))
-        # plt.show()
-
-        cv2.imshow("heatmap", heatmapImage)
-        cv2.waitKey(10)
-        time.sleep(10)
+    print(setLightLevel())
+    # while True:
+    #     multiplier = 4
+    #     heatmapImage, heatmapResolution = getHeatmapHeatmapImage()
+    #     heatmapImage = cv2.resize(heatmapImage, (heatmapResolution[1]*multiplier, heatmapResolution[0]*multiplier), interpolation=cv2.INTER_CUBIC)
+    #     # plt.imshow(heatmapImage)
+    #     # plt.title(datetime.datetime.now().strftime("%H:%M:%S"))
+    #     # plt.show()
+    #
+    #     cv2.imshow("heatmap", heatmapImage)
+    #     cv2.waitKey(10)
+    #     time.sleep(10)
 
 
 if (__name__ == "__main__"):
