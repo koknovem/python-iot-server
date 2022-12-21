2ab5fd008703353fab9841271105ad0953bec3da
Brian Li <brianli@master-dynamic.com>
2022-12-21 21:07:16 +0800

    videowriter

---
+++ videowrite.py
@@ -0,0 +1,20 @@
+import cv2
+from api import rtspStream
+
+cap = rtspStream()
+# Get image width and height
+image_width, image_height = int(cap.get(3)), int(cap.get(4))
+
+# Video Write
+out = cv2.VideoWriter(f"./WSP_raw_video.mp4",
+                      cv2.VideoWriter_fourcc(*'mp4v'), 30,
+                      (image_width, image_height))
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    out.write(frame)
+
